809352dc022d4c22e4985cadd9f748b3c405b6c7
developerjarvis <abhinavsharma27852862@gmail.com>
2025-12-04 21:21:36 +0530

    updated .c and .h

--- tic_tac_toe.h
+++ tic_tac_toe.h
@@ -3,7 +3,7 @@
 //
 // Copyright (C) 2023 Developer Jarvis (Pen Name)
 //
-// This file is part of the [Project Name] Library. This library is free
+// This file is part of the tic_tac_toe Library. This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
